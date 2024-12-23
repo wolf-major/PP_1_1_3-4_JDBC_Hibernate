16778a472769468822105787e09e7be94b5c4697
Daria Prudnikova <daria2997@gmail.com>
2024-12-23 17:24:41 +0300

    Kata_preproject_1.1.3

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,17 +1,24 @@
 package jm.task.core.jdbc;
 
-import jm.task.core.jdbc.model.User;
-import jm.task.core.jdbc.service.UserServiceImpl;
+import jm.task.core.jdbc.dao.UserDao;
+import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
 import jm.task.core.jdbc.util.Util;
 
 public class Main {
     public static void main(String[] args) {
-        // реализуйте алгоритм здесь
-        Util util = new Util();
         Util.getConnection();
+        UserDao userDao = new UserDaoJDBCImpl();
 
-        UserServiceImpl userService = new UserServiceImpl();
-        userService.createUsersTable();
-        //userService.saveUser("Иван", "Безухов", (byte) 15);
+        userDao.createUsersTable();
+
+        userDao.saveUser("Alex", "Nesterov", (byte) 21);
+        userDao.saveUser("Kate", "Smith", (byte) 35);
+        userDao.saveUser("Lola", "Winter", (byte) 21);
+        userDao.saveUser("James", "Pupkin", (byte) 55);
+
+        System.out.println(userDao.getAllUsers().toString());
+        userDao.cleanUsersTable();
+        userDao.dropUsersTable();
     }
+
 }
