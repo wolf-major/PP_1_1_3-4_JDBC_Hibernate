8619577bd6e146a8ec787f9d1a16f02e4993de3f
Daria Prudnikova <daria2997@gmail.com>
2025-01-05 17:25:10 +0300

    ver. 05/01/25

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -9,33 +9,26 @@ public class UserDaoHibernateImpl implements UserDao {
 
     }
 
-
-    @Override
     public void createUsersTable() {
 
     }
 
-    @Override
     public void dropUsersTable() {
 
     }
 
-    @Override
     public void saveUser(String name, String lastName, byte age) {
 
     }
 
-    @Override
     public void removeUserById(long id) {
 
     }
 
-    @Override
     public List<User> getAllUsers() {
         return null;
     }
 
-    @Override
     public void cleanUsersTable() {
 
     }
