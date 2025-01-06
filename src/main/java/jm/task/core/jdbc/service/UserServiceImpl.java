4cbb20d9a9cb1c40cc7fd271ac1706f660e45240
Daria Prudnikova <daria2997@gmail.com>
2025-01-06 12:32:06 +0300

    fixed tabs

--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -13,26 +13,32 @@ public class UserServiceImpl implements UserService {
         this.userDao = new UserDaoJDBCImpl();
     }
 
+    @Override
     public void createUsersTable() {
         userDao.createUsersTable();
     }
 
+    @Override
     public void dropUsersTable() {
         userDao.dropUsersTable();
     }
 
+    @Override
     public void saveUser(String name, String lastName, byte age) {
         userDao.saveUser(name, lastName, age);
     }
 
+    @Override
     public void removeUserById(long id) {
         userDao.removeUserById(id);
     }
 
+    @Override
     public List<User> getAllUsers() {
         return userDao.getAllUsers();
     }
 
+    @Override
     public void cleanUsersTable() {
         userDao.cleanUsersTable();
     }
